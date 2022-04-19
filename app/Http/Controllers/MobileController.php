0ed7f53c56e853e76f72f2145edbc273cbc1d202
favizille <favourgabriel50@gamail.com>
2022-04-19 13:44:38 +0100

    favour favour favour favour

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MobileController extends Controller
 {
-    //
+    public function register(Request $request){
+        $fields = $request->validate([
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'email'=>'required',
+            'phone_number'=>'required',
+            'country'=>'required',
+            'password'=>'required',
+        ]);
+
+        $user = User::create([
+            'first_name' => $fields['first_name'],
+            'last_name' => $fields['last_name'],
+            'email' => $fields ['email'],
+            'phone_number' => $fields ['phone_number'],
+            'country' => $fields ['country'],
+            'password' => bcrypt($fields['password']),
+        ]);
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+}
 }
